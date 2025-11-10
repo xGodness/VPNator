3b0e8f9732afead1430ce9ab608952cdd46970e4
Максим Давыдов <maxim.davydov@vkteam.ru>
2025-11-11 00:43:10 +0300

    Добавил карточку

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import {
   AppRoot,
   Box,
   Button,
+  Card,
   Div,
   Flex,
   FormItem,
@@ -14,6 +15,7 @@ import {
   SplitCol,
   SplitLayout,
   Title,
+  usePlatform,
   View,
 } from "@vkontakte/vkui";
 
@@ -35,43 +37,60 @@ const vpnProtocols = [
 ];
 
 export default function App() {
+  const platform = usePlatform();
+
   return (
     <AppRoot disableSettingVKUIClassesInRuntime>
-      <Root activeView="form-view">
-        <View id="form-view" activePanel="form-panel">
-          <Panel id="form-panel" mode="card">
-            <PanelHeader>VPNator</PanelHeader>
-            <Flex className={styles.form} direction="column">
-              <FormLayoutGroup mode="vertical">
-                <FormItem htmlFor="address" top="Адрес">
-                  <Input name="address" id="address" />
-                </FormItem>
-                <FormItem htmlFor="protocol" top="Протокол">
-                  <Select
-                    options={vpnProtocols}
-                    defaultValue={null}
-                    name="protocol"
-                    id="protocol"
-                  />
-                </FormItem>
-                <FormLayoutGroup mode="horizontal">
-                  <FormItem htmlFor="username" top="Имя пользователя">
-                    <Input name="username" id="username" />
-                  </FormItem>
-                  <FormItem htmlFor="password" top="Пароль">
-                    <Input name="password" id="password" type="password" />
-                  </FormItem>
-                </FormLayoutGroup>
-              </FormLayoutGroup>
-              <Box padding="2xl">
-                <Button type="submit" size="m">
-                  Настроить
-                </Button>
-              </Box>
-            </Flex>
-          </Panel>
-        </View>
-      </Root>
+      <SplitLayout
+        header={platform !== "vkcom" && <PanelHeader delimiter="none" />}
+      >
+        <SplitCol stretchedOnMobile autoSpaced>
+          <Root activeView="form-view">
+            <View id="form-view" activePanel="form-panel">
+              <Panel id="form-panel">
+                <PanelHeader>VPNator</PanelHeader>
+                <Card mode="shadow">
+                  <Box padding="2xl">
+                    <Title level="2">Настройка конфига</Title>
+                  </Box>
+                  <Flex className={styles.form} direction="column">
+                    <FormLayoutGroup mode="vertical">
+                      <FormItem htmlFor="address" top="Адрес">
+                        <Input name="address" id="address" />
+                      </FormItem>
+                      <FormItem htmlFor="protocol" top="Протокол">
+                        <Select
+                          options={vpnProtocols}
+                          defaultValue={null}
+                          name="protocol"
+                          id="protocol"
+                        />
+                      </FormItem>
+                      <FormLayoutGroup mode="horizontal">
+                        <FormItem htmlFor="username" top="Имя пользователя">
+                          <Input name="username" id="username" />
+                        </FormItem>
+                        <FormItem htmlFor="password" top="Пароль">
+                          <Input
+                            name="password"
+                            id="password"
+                            type="password"
+                          />
+                        </FormItem>
+                      </FormLayoutGroup>
+                    </FormLayoutGroup>
+                    <Box padding="2xl">
+                      <Button type="submit" size="m">
+                        Настроить
+                      </Button>
+                    </Box>
+                  </Flex>
+                </Card>
+              </Panel>
+            </View>
+          </Root>
+        </SplitCol>
+      </SplitLayout>
     </AppRoot>
   );
 }
