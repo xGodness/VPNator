7d80c06dd5dad37ee2131e0c5b9cbadbadbf780c
Максим Давыдов <maxim.davydov@vkteam.ru>
2025-11-11 00:33:15 +0300

    Добавил panel view и root

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,10 +7,14 @@ import {
   FormItem,
   FormLayoutGroup,
   Input,
+  Panel,
+  PanelHeader,
+  Root,
   Select,
   SplitCol,
   SplitLayout,
   Title,
+  View,
 } from "@vkontakte/vkui";
 
 import styles from "./App.module.css";
@@ -33,41 +37,41 @@ const vpnProtocols = [
 export default function App() {
   return (
     <AppRoot disableSettingVKUIClassesInRuntime>
-      <SplitLayout>
-        <SplitCol stretchedOnMobile autoSpaced>
-          <Box padding="2xl">
-            <Title>VPNator</Title>
-          </Box>
-          <Flex className={styles.form} direction="column">
-            <FormLayoutGroup mode="vertical">
-              <FormItem htmlFor="address" top="Адрес">
-                <Input name="address" id="address" />
-              </FormItem>
-              <FormItem htmlFor="protocol" top="Протокол">
-                <Select
-                  options={vpnProtocols}
-                  defaultValue={null}
-                  name="protocol"
-                  id="protocol"
-                />
-              </FormItem>
-              <FormLayoutGroup mode="horizontal">
-                <FormItem htmlFor="username" top="Имя пользователя">
-                  <Input name="username" id="username" />
+      <Root activeView="form-view">
+        <View id="form-view" activePanel="form-panel">
+          <Panel id="form-panel" mode="card">
+            <PanelHeader>VPNator</PanelHeader>
+            <Flex className={styles.form} direction="column">
+              <FormLayoutGroup mode="vertical">
+                <FormItem htmlFor="address" top="Адрес">
+                  <Input name="address" id="address" />
                 </FormItem>
-                <FormItem htmlFor="password" top="Пароль">
-                  <Input name="password" id="password" type="password" />
+                <FormItem htmlFor="protocol" top="Протокол">
+                  <Select
+                    options={vpnProtocols}
+                    defaultValue={null}
+                    name="protocol"
+                    id="protocol"
+                  />
                 </FormItem>
+                <FormLayoutGroup mode="horizontal">
+                  <FormItem htmlFor="username" top="Имя пользователя">
+                    <Input name="username" id="username" />
+                  </FormItem>
+                  <FormItem htmlFor="password" top="Пароль">
+                    <Input name="password" id="password" type="password" />
+                  </FormItem>
+                </FormLayoutGroup>
               </FormLayoutGroup>
-            </FormLayoutGroup>
-            <Box padding="2xl">
-              <Button type="submit" size="m">
-                Настроить
-              </Button>
-            </Box>
-          </Flex>
-        </SplitCol>
-      </SplitLayout>
+              <Box padding="2xl">
+                <Button type="submit" size="m">
+                  Настроить
+                </Button>
+              </Box>
+            </Flex>
+          </Panel>
+        </View>
+      </Root>
     </AppRoot>
   );
 }
